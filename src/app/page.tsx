df18c8257821d4f27299db2a84f231b73eb41229
365-Code <sohailahmedbhati786@gmail.com>
2024-02-20 23:15:50 +0530

    home page updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,19 @@ import Hero from "@/components/Hero";
 import Latest from "@/components/Latest";
 import NewSection from "@/components/NewSection";
 import ShopProducts from "@/components/ShopProducts";
+import Subscribe from "@/components/Subscribe";
 import React from "react";
 
 const Home = () => {
   return (
-    <div className="relative">
+    <main className="relative">
       <Hero/>
       <Latest />
       <Companies/>
       <NewSection/>
       <ShopProducts />
-    </div>
+      <Subscribe/>
+    </main>
   );
 };
 
