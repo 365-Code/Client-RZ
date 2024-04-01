c333dbd3380a8a71044cf3857a2abf804677d0c6
365-Code <sohailahmedbhati786@gmail.com>
2024-04-01 15:00:27 +0530

    some change - 4pm

--- src/app/api/product/fetchAllProducts/route.ts
+++ src/app/api/product/fetchAllProducts/route.ts
@@ -6,10 +6,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest){
   try {
     await connectDB()
-    const {searchParams} = new URL(req.url)
     const products = await productModel.find().populate("category")
-    console.log(products);
-    
     return NextResponse.json({success: true, products}, {status: 200})
   } catch (error) {
     return NextResponse.json({success: false}, {status: 500, statusText: "Internal Server Error. Couldn't fetch products"})
